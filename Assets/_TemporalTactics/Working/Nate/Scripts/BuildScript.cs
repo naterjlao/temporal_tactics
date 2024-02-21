11f6a1544bc8fe1958f21f9b9606c054e4c7e485
natelao <lao.nathan@yahoo.com>
2024-02-20 23:31:45 -0600

    add buildscript

---
+++ Assets/_TemporalTactics/Working/Nate/Scripts/BuildScript.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+
+public static class BuildScript
+{
+    // These must be updated on a project-by-project basis.
+    private static string NAME = "TemporalTactics";
+    private static string[] SCENES =
+    {
+        "Assets/_Temporal Tactics/Integrated/Scenes/Hello World Cube.unity"
+    };
+
+    private static void Build(string[] scenes, string locationPathName, BuildTarget target)
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = locationPathName,
+            target = target
+        };
+        BuildPipeline.BuildPlayer(options);
+    }
+
+    public static void BuildWindows()
+    {
+        Build(SCENES, $"Builds/Windows/{NAME}.exe", BuildTarget.StandaloneWindows64);
+    }
+
+    public static void BuildMacOSX()
+    {
+        Build(SCENES, $"Builds/MacOSX/{NAME}", BuildTarget.StandaloneOSX);
+    }
+
+    public static void BuildAll()
+    {
+        BuildWindows();
+        BuildMacOSX();
+    }
+}
