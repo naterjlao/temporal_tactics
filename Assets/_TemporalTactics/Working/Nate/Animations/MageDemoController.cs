db8968e2e72b835a8562c057a72f61f22d64ae95
Nate <lao.nathan@yahoo.com>
2024-03-10 20:45:01 -0500

    movement

--- Assets/_TemporalTactics/Working/Nate/Animations/MageDemoController.cs
+++ Assets/_TemporalTactics/Working/Nate/Animations/MageDemoController.cs
@@ -1,13 +1,18 @@
+using System;
 using UnityEngine;
 
 public class MageDemoController : MonoBehaviour
 {
-    Animator animator;
-    int movement_speed_hash;
+    private CharacterController controller;
+    private Animator animator;
+    private int movement_speed_hash;
+
+    public float BASE_SPEED = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         // Optimize: reduce lookup overhead
         movement_speed_hash = Animator.StringToHash("movement_speed");
@@ -16,23 +21,31 @@ public class MageDemoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int current_speed = animator.GetInteger(movement_speed_hash);
-        int command_speed = 0;
+        // Technically, this is a keyboard only game - so this might not be necessary
+        float AXIS_THRESHOLD = .5f;
+        float h_axis = Input.GetAxis("Horizontal");
+        float v_axis = Input.GetAxis("Vertical");
+
+        Debug.Log(h_axis + " " + v_axis);
 
         // Calculate movement speed - only run if we are walking.
-        if (Input.GetKey("w"))
+        // REF: https://www.youtube.com/watch?v=-FhvQDqmgmU
+        int command_speed = 0;
+        if ((Math.Abs(h_axis) > AXIS_THRESHOLD) || (Math.Abs(v_axis) > AXIS_THRESHOLD))
         {
             command_speed += 1;
             if (Input.GetKey("space"))
-            {
                 command_speed += 1;
-            }
         }
 
+        int current_speed = animator.GetInteger(movement_speed_hash);
         // Optimize: only use setter if there is a change of state
         if (current_speed != command_speed)
         {
             animator.SetInteger(movement_speed_hash, command_speed);
         }
+
+        Vector3 move = new Vector3(h_axis, 0, v_axis);
+        controller.Move(move * Time.deltaTime * BASE_SPEED * command_speed);
     }
 }
