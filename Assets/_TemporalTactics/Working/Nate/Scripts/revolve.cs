e505e1a897ce401f4a06f67eb79ff9ef2c65406f
Nate <lao.nathan@yahoo.com>
2024-04-13 15:13:58 -0500

    hide the mini world for char select

--- Assets/_TemporalTactics/Working/Nate/Scripts/revolve.cs
+++ Assets/_TemporalTactics/Working/Nate/Scripts/revolve.cs
@@ -1,22 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Dreamteck.Splines.Editor;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class revolve : MonoBehaviour
 {
-    public float speed = 25.0f;
+    public float revolve_speed = 10;
+    public float transition_speed = 3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private float on_screen = -440;
+    private float off_screen = -600;
+    private float dest = -440;
     void Update()
     {
-        //transform.localEulerAngles = transform.localEulerAngles + (new Vector3(0,1,0) *Time.deltaTime);
-        transform.Rotate(0,speed * Time.deltaTime,0);
+        transform.Rotate(0,revolve_speed * Time.deltaTime,0);
+
+        float error = dest - transform.localPosition.y;
+        if (Math.Abs(error) > 1.0)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * error * transition_speed);
+        }
+    }
+
+    [Button]
+    public void Hide()
+    {
+        dest = off_screen;
+    }
+
+    [Button]
+    public void Show()
+    {
+        dest = on_screen;
     }
 }
