1b2aee245767c67772dba4731c456b0ea42d3059
Nate <lao.nathan@yahoo.com>
2024-04-13 14:53:09 -0500

    simple character select

--- Assets/_TemporalTactics/Working/Nate/Scripts/CharacterSelection.cs
+++ Assets/_TemporalTactics/Working/Nate/Scripts/CharacterSelection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -8,18 +9,80 @@ public class CharacterSelection : MonoBehaviour
 {
     public GameObject[] characters;
     public int selection = 0;
-
-    [Button]
-    public void Next()
+    public void Start()
     {
-        characters[selection].SetActive(false);
-        selection = (selection + 1) % characters.Length;
-        characters[selection].SetActive(true);
+        // Set only the default character visible.
+        for (int idx = 0; idx < characters.Length; idx++)
+        {
+            characters[idx].SetActive(idx == selection);
+        }
+    }
+
+    public float speed = 3;
+    private float screen_right = -50;
+    private float screen_middle = -330;
+    private float screen_left = -580;
+    private float dest = -580;
+    public void Update()
+    {
+        float error = dest - transform.localPosition.x;
+        transform.localPosition = new Vector3(
+            transform.localPosition.x + (speed * Time.deltaTime * error),
+            transform.localPosition.y,
+            transform.localPosition.z
+        );
+    }
+
+    public bool atDest()
+    {
+        return Math.Abs(dest - transform.localPosition.x) < 20;
     }
 
     [Button]
-    public void Prev()
+    public void flyLeft()
     {
+        dest = screen_left;
+    }
+
+    [Button]
+    public void flyMiddle()
+    {
+        dest = screen_middle;
+    }
+
+    [Button]
+    public void flyRight()
+    {
+        dest = screen_right;
+    }
+
+    [Button]
+    IEnumerator Next()
+    {
+        // Fly out
+        flyRight();
+        yield return new WaitUntil(atDest);
+
+        // Change out character
+        characters[selection].SetActive(false);
+        selection = (selection + 1) % characters.Length;
+        characters[selection].SetActive(true);
+
+        // Fly in
+        transform.localPosition = new Vector3(
+            screen_left,
+            transform.localPosition.y,
+            transform.localPosition.z);
+        flyMiddle();
+    }
+
+    [Button]
+    IEnumerator Prev()
+    {
+        // Fly out
+        flyLeft();
+        yield return new WaitUntil(atDest);
+
         characters[selection].SetActive(false);
         selection = selection - 1;
         if (selection < 0)
@@ -27,5 +90,12 @@ public class CharacterSelection : MonoBehaviour
             selection = characters.Length - 1;
         }
         characters[selection].SetActive(true);
+
+        // Fly in
+        transform.localPosition = new Vector3(
+            screen_right,
+            transform.localPosition.y,
+            transform.localPosition.z);
+        flyMiddle();
     }
 }
